7a4a29ee1503a91d9f7537c4e807fc9ef0138f74
Jose Villegas <jose123millon@gmail.com>
2024-02-04 00:09:31 -0500

    Add getCategoryById, updateCategory, and deleteCategory controllers

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -11,15 +11,32 @@ import { Context } from "hono";
 export async function getCategories(c: Context) {
 	// Get all categories from the database
 	const categories: Array<Category> = await prisma.category.findMany();
-
 	if (!categories) {
 		c.notFound();
 	}
-
 	// Return all the categories
 	return c.json(categories);
 }
 
+
+export async function getCategoryById(c: Context) {
+	// Get the id from the request parameters
+	const { id } = c.req.param();
+
+	// Get the category by id
+	const category = await prisma.category.findUnique({
+		where: { id: id },
+	});
+
+	// If the model does not exist, return an error
+	if (!category) {
+		return c.notFound();
+	}
+
+	// Return the category
+	return c.json(category);
+
+}
 
 /**
  * Create Category Controller function that extract the name and description from the request body and create a new category in the database.
@@ -58,3 +75,73 @@ export async function createCategory(c: Context) {
 	return c.json(category, 201);
 }
 
+
+/**
+ * Get Category Controller function that extract the id from the request parameters and return the category from the database.
+ * @param c Context parameter from Hono
+ * @returns return the updated category from the database
+ */
+export async function updateCategory(c: Context) {
+
+	// Get the id from the request parameters and the name and description from the request body
+	const { id } = c.req.param();
+	const body: Category = await c.req.json();
+	const { name, description } = body;
+
+	// Validate the name or description
+	if (!name && !description) {
+		return c.json({ error: "Name or description is required" }, 400);
+	}
+
+	// Check if the category already exists
+	const findCategory = await prisma.category.findUnique({
+		where: {
+			id
+		},
+	});
+
+	// If the category does not exists, return an error
+	if (!findCategory) {
+		return c.notFound();
+	}
+
+	// if the object is the same, return the object
+	if (findCategory.name === name && findCategory.description === description) {
+		return c.json(findCategory);
+	}
+
+	// Update the category in database
+	const updatedCategory = await prisma.category.update({
+		where: { id },
+		data: {
+			name,
+			description,
+		},
+	});
+
+	// Response with the updated category
+	return c.json(updatedCategory);
+}
+
+/**
+ * Get Category Controller function that extract the id from the request parameters and return the category from the database.
+ * @param c Context parameter from Hono
+ * @returns return the deleted category from the database
+ */
+export async function deleteCategory(c: Context) {
+	const { id } = c.req.param();
+
+	const category = await prisma.category.findUnique({
+		where: { id },
+	});
+
+	if (!category) {
+		return c.notFound();
+	}
+
+	const deletedCategory = await prisma.category.delete({
+		where: { id },
+	});
+
+	return c.json(deletedCategory);
+}
--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -1,13 +1,26 @@
-import { createCategory, getCategories } from "@/controllers/categoriesController";
+// Import Controllers
+import { createCategory, deleteCategory, getCategories, updateCategory, getCategoryById } from "@/controllers/categoriesController";
+// Import middlewares
 import { createSortMiddleware } from "@/middlewares/createSortMiddleware";
-import { prisma } from "@/lib/prisma";
+
+
+// Import types
 import type { Category, Prisma } from "@prisma/client";
+
+// Import dependencies
 import { Hono } from "hono";
+import { prisma } from "@/lib/prisma";
+
 
 export const categoriesRouter = new Hono();
 
 // get all categories
 categoriesRouter.get("/", createSortMiddleware<Prisma.CategoryDelegate, Category>(prisma.category, "name"), getCategories);
-
+// Get categrory by id
+categoriesRouter.get("/:id", getCategoryById)
 // Create a new category
 categoriesRouter.post("/", createCategory);
+// Update a category
+categoriesRouter.put("/:id", updateCategory);
+// Delete a category
+categoriesRouter.delete("/:id", deleteCategory);
